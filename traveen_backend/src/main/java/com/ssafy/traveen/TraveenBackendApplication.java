b8b61bb0886376c0356a037f4497e2599d404170
SSAFY-GitLab kn9012 <SSAFY-GitLab kn9012@naver.com>
2023-10-31 21:44:16 +0900

    [FEAT] Backend project setting

---
+++ traveen_backend/src/main/java/com/ssafy/traveen/TraveenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.traveen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveenBackendApplication.class, args);
+	}
+
+}
---
+++ traveen_backend/src/test/java/com/ssafy/traveen/TraveenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.traveen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraveenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
