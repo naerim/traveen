5b4d296c8ca79cebd218356be8d06b5513476535
naerim <naerim1119@gmail.com>
2023-11-03 23:18:56 +0900

    [FEAT] interceptor 설정

--- traveen_backend/src/main/java/com/ssafy/config/WebMvcConfiguration.java
+++ traveen_backend/src/main/java/com/ssafy/config/WebMvcConfiguration.java
@@ -3,13 +3,16 @@ package com.ssafy.config;
 import java.util.Arrays;
 import java.util.List;
 
+import com.ssafy.interceptor.ConfirmInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,10 +26,10 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	
 	private final Logger logger = LoggerFactory.getLogger(WebMvcConfiguration.class);
 
-//	private final List<String> patterns = Arrays.asList("/attendance/*", "/admin", "/user/list");
+	private final List<String> patterns = Arrays.asList("/notice/*", "/admin", "/user/myinfo");
 
-//	@Autowired
-//	private ConfirmInterceptor confirmInterceptor;
+	@Autowired
+	private ConfirmInterceptor confirmInterceptor;
 
 //	private final String uploadFilePath;
 
@@ -47,9 +50,9 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	}
 
 //	@Override
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(confirmInterceptor).addPathPatterns(patterns);
-//	}
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(confirmInterceptor).addPathPatterns(patterns);
+	}
 
 //	@Override
 //	public void addResourceHandlers(ResourceHandlerRegistry registry) {
