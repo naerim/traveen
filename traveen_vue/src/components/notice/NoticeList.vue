88e039566f5a1164290902006164dbe68bc81055
김유나 <kn9012@naver.com>
2023-11-10 09:05:18 +0900

    [CHORE] vue notice 목록 수정

--- traveen_vue/src/components/notice/NoticeList.vue
+++ traveen_vue/src/components/notice/NoticeList.vue
@@ -1,85 +1,82 @@
-<script setup>
-import TheHeadingNavbar from "@/components/layout/TheHeadingNavbar.vue";
-</script>
+<script setup></script>
 
 <template>
   <div>공지사항 페이지</div>
-  <the-heading-navbar></the-heading-navbar>
 
   <!-- section -->
-	<section>
-		<h2 class="title">공지사항</h2>
-		<h4 class="subtitle">Traveen의 공지사항을 확인해보세요.</h4>
-		<!-- top-box -->
-		<div class="top-box">
-			<div class="left">총 ${totalCnt}건</div>
-			<form class="right" id="form-search" method="get">
-				<input type="hidden" name="pgno" value="1" />
-				<select
-					name="key" id="key">
-					<option value="">전체</option>
-					<option value="title">제목</option>
-				</select> <input type="text" name="word" id="word"
-					placeholder="검색어를 입력해주세요." />
-					<button id="btn-search" type="button">검색</button>
-				<button type="button" id="btn-regist" style="<c:if test="${userinfo.position ne M}">display: none</c:if>">글 등록</button>
-				
-		</div>
-		<div class="table-container">
-			<!-- table -->
-			<table>
-				<!-- thead -->
-				<thead>
-					<tr>
-						<th width="8%">NO.</th>
-						<th width="67%">제목</th>
-						<th width="15%">게시일</th>
-						<th width="10%">조회수</th>
-					</tr>
-				</thead>
-				<!-- tbody -->
-				<tbody>
-
-					<c:if test="${not empty notices}">
-						<c:forEach var="notice" items="${notices}" varStatus="status">
-							<tr>
-								<td>${fn:length(notices) - status.index}</td>
-									<td class="subject"><a
-									href=# class="notice-title link-dark" data-no="${notice.idx}">${notice.title}</a></td>
-								<td><c:out
-										value="${fn:substring(notice.createDate, 0, 10)}" /></td>
-								<td>${notice.viewCount}</td>
-							</tr>
-						</c:forEach>
-					</c:if>
-
-					<c:if test="${empty notices}">
-						<tr>
-							<td colspan="5">작성된 글이 없습니다.</td>
-						</tr>
-					</c:if>
-
-				</tbody>
-			</table>
-			</form>
-		</div>
-		<!-- pagination -->
-		<div class="pagination-container">
-        	${navigation.navigator}
-      	</div>
-	</section>
-
-	<form id="form-param" method="get" action="">
-		<input type="hidden" name="pgno" id="pgno" value="${pgno}">
-	</form>
-	<form id="form-no-param" method="get" action="${root}/notice/view">
-	  <input type="hidden" name="pgno" value="${pgno}">
-	  <input type="hidden" name="key" value="${key}">
-	  <input type="hidden" name="word" value="${word}">
-	  <input type="hidden" id="idx" name="idx" value="">
-	</form>
+  <section>
+    <h2 class="title">공지사항</h2>
+    <h4 class="subtitle">Traveen의 공지사항을 확인해보세요.</h4>
+    <!-- top-box -->
+    <div class="top-box">
+      <div class="left">총 4건</div>
+      <form class="right" id="form-search" action="#" method="post">
+        <select name="select-notice" id="select-notice">
+          <option value="">전체</option>
+          <option value="subject">제목</option>
+        </select>
+        <input type="text" name="keyword" id="keyword" placeholder="검색어를 입력해주세요." />
+        <input type="submit" id="btn-search" value="검색" />
+        <button id="btn-insert">글 등록</button>
+      </form>
+    </div>
+    <div class="table-container">
+      <!-- table -->
+      <table>
+        <!-- thead -->
+        <thead>
+          <tr>
+            <th width="8%">NO.</th>
+            <th width="67%">제목</th>
+            <th width="15%">게시일</th>
+            <th width="10%">조회수</th>
+          </tr>
+        </thead>
+        <!-- tbody -->
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td class="subject">
+              <a href="./notice-detail.html">트래빈 전체 공지사항 입니다.</a>
+            </td>
+            <td>2023.11.01</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td class="subject">
+              <a href="#">트래빈 전체 공지사항 입니다.</a>
+            </td>
+            <td>2023.11.01</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td class="subject">트래빈 전체 공지사항 입니다.</td>
+            <td>2023.11.01</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td class="subject">트래빈 전체 공지사항 입니다.</td>
+            <td>2023.11.01</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- pagination -->
+    <div class="pagination-container">
+      <div class="btn-prev">&#60;이전</div>
+      <span class="divider"></span>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+      </ul>
+      <span class="divider"></span>
+      <div class="btn-next">다음&#62;</div>
+    </div>
+  </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- traveen_vue/src/views/NoticeView.vue
+++ traveen_vue/src/views/NoticeView.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+import TheHeadingNavbar from '../components/layout/TheHeadingNavbar.vue';
+import TheFooter from '../components/layout/TheFooter.vue';
+import NoticeList from '../components/notice/NoticeList.vue';
+</script>
 
 <template>
   <div>공지사항 페이지</div>
+  <TheHeadingNavbar></TheHeadingNavbar>
+  <NoticeList></NoticeList>
+  <TheFooter></TheFooter>
 </template>
 
 <style scoped></style>
