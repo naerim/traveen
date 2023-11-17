464b8517dd426bdc30e2bf4fa5d86d3db2995188
naerim <naerim1119@naver.com>
2023-11-17 17:57:38 +0900

    [FEAT] 나의 친구 tabMenu demo

---
+++ traveen_vue/src/components/myfriend/Following.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="tab">following</div>
+</template>
+
+<style scoped></style>
--- traveen_vue/src/views/MyFriendView.vue
+++ traveen_vue/src/views/MyFriendView.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+import { shallowRef } from "vue";
+import Follower from "../components/myfriend/Follower.vue";
+import Following from "../components/myfriend/Following.vue";
+
+const current = shallowRef(Follower);
+const change = (val) => {
+  current.value = val == "follower" ? Follower : Following;
+};
+</script>
 
 <template>
   <div>나의 친구</div>
+  <button @click="change('follower')">follower</button>
+  <button @click="change('following')">following</button>
+  <component :is="current"></component>
 </template>
 
 <style scoped></style>
