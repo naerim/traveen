e96eb436277bb79197a517f8aa84ac45aa4037a0
naerim <naerim1119@gmail.com>
2023-12-28 22:10:05 +0900

    [CHORE] 코딩컨벤션 적용 실습

--- traveen_backend/src/main/java/com/ssafy/util/UserUtil.java
+++ traveen_backend/src/main/java/com/ssafy/util/UserUtil.java
@@ -7,39 +7,39 @@ import java.security.SecureRandom;
 public class UserUtil {
 	 public String getSalt() {
 	        //1. Random, salt 생성
-	        SecureRandom sr = new SecureRandom();
+	        SecureRandom secureRandom = new SecureRandom();
 	        byte[] salt = new byte[20];
 
 	        //2. 난수 생성
-	        sr.nextBytes(salt);
+		 	secureRandom.nextBytes(salt);
 
 	        //3. byte To String (10진수 문자열로 변경)
-	        StringBuffer sb = new StringBuffer();
-	        for(byte b : salt) {
-	            sb.append(String.format("%02x", b));
+	        StringBuffer stringBuffer = new StringBuffer();
+	        for(byte byteSalt : salt) {
+				stringBuffer.append(String.format("%02x", byteSalt));
 	        }
 
-	        return sb.toString();
+	        return stringBuffer.toString();
 	    }
 
 	  	// SHA-256 알고리즘 적용
 	    public String getEncrypt(String pwd, String salt) {
 	        String result = "";
 	        try {
-	            MessageDigest md = MessageDigest.getInstance("SHA-256");
+	            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
 
-	            md.update((pwd + salt).getBytes());
-	            byte[] pwdSalt = md.digest();
+				messageDigest.update((pwd + salt).getBytes());
+	            byte[] pwdSalt = messageDigest.digest();
 
-	            StringBuffer sb = new StringBuffer();
-	            for(byte b : pwdSalt) {
-	                sb.append(String.format("%02x", b));
+	            StringBuffer stringBuffer = new StringBuffer();
+	            for(byte bytePwdSalt : pwdSalt) {
+					stringBuffer.append(String.format("%02x", bytePwdSalt));
 	            }
 
-	            result = sb.toString();
+	            result = stringBuffer.toString();
 
-	        } catch (NoSuchAlgorithmException e) {
-	            throw new RuntimeException(e);
+	        } catch (NoSuchAlgorithmException error) {
+	            throw new RuntimeException(error);
 	        }
 
 	        return result;
